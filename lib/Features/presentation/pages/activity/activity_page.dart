e9b7ba3971fdc3b509fdcfa28c6c62f771db9448
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2023-12-11 15:58:35 +0200

    create activity page

---
+++ lib/Features/presentation/pages/activity/activity_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../../../../consts.dart';
+
+class ActivityPage extends StatelessWidget {
+  const ActivityPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backGroundColor,
+        title: const Text(
+          'Activity',
+          style: TextStyle(
+            color: primaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
