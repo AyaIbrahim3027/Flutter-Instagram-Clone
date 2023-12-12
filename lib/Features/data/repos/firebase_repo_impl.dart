cf724fcb744dfbbbfbdd5d7fe11491873fbaf9c4
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2023-12-12 15:41:56 +0200

    create firebase repo impl

---
+++ lib/Features/data/repos/firebase_repo_impl.dart
@@ -0,0 +1,46 @@
+import 'package:instagram_clone/Features/data/data%20sources/remote_data_source/remote_data_source.dart';
+import 'package:instagram_clone/Features/domain/entities/user/user_entity.dart';
+import 'package:instagram_clone/Features/domain/repos/firebase_repo.dart';
+
+class FirebaseRepoImpl implements FirebaseRepo{
+  final FirebaseRemoteDataSource remoteDataSource;
+
+  FirebaseRepoImpl({required this.remoteDataSource});
+
+  @override
+  Future<void> createUser(UserEntity user) async =>
+      remoteDataSource.createUser(user);
+
+  @override
+  Future<String> getCurrentUserId() async =>
+      remoteDataSource.getCurrentUserId();
+
+  @override
+  Stream<List<UserEntity>> getSingleUser(String userId) =>
+      remoteDataSource.getSingleUser(userId);
+
+  @override
+  Stream<List<UserEntity>> getUsers(UserEntity user) =>
+      remoteDataSource.getUsers(user);
+
+  @override
+  Future<bool> isSignIn() async =>
+      remoteDataSource.isSignIn();
+
+  @override
+  Future<void> signInUser(UserEntity user) async =>
+      remoteDataSource.signInUser(user);
+
+  @override
+  Future<void> signOut() async =>
+      remoteDataSource.signOut();
+
+  @override
+  Future<void> signUpUser(UserEntity user) async =>
+      remoteDataSource.signUpUser(user);
+
+  @override
+  Future<void> updateUser(UserEntity user) async =>
+      remoteDataSource.updateUser(user);
+  
+}
