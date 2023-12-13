52178bd5df46da8ad916d5a8448b605d31c81621
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2023-12-13 12:05:45 +0200

    create Firebase Remote Data Source Impl

---
+++ lib/Features/data/data sources/remote_data_source/remote_data_source_impl.dart
@@ -0,0 +1,138 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:instagram_clone/Features/data/data%20sources/remote_data_source/remote_data_source.dart';
+import 'package:instagram_clone/Features/domain/entities/user/user_entity.dart';
+import 'package:instagram_clone/consts.dart';
+
+import '../../models/user/user_model.dart';
+
+class FirebaseRemoteDataSourceImpl implements FirebaseRemoteDataSource {
+  final FirebaseFirestore firebaseFirestore;
+  final FirebaseAuth firebaseAuth;
+
+  FirebaseRemoteDataSourceImpl({
+    required this.firebaseFirestore,
+    required this.firebaseAuth,
+  });
+
+  @override
+  Future<void> createUser(UserEntity user) async {
+    final userCollection = firebaseFirestore.collection(FirebaseConst.users);
+    final userId = await getCurrentUserId();
+    userCollection.doc(userId).get().then((userDoc) {
+      final newUser = UserModel(
+              userId: userId,
+              name: user.name,
+              email: user.email,
+              bio: user.bio,
+              following: user.following,
+              website: user.website,
+              profileUrl: user.profileUrl,
+              userName: user.userName,
+              totalFollowers: user.totalFollowers,
+              followers: user.followers,
+              totalFollowing: user.totalFollowing,
+              totalPosts: user.totalPosts)
+          .tpJson();
+
+      if (!userDoc.exists) {
+        userCollection.doc(userId).set(newUser);
+      } else {
+        userCollection.doc(userId).update(newUser);
+      }
+    }).catchError((error) {
+      toast('Some error occur');
+    });
+  }
+
+  @override
+  Future<String> getCurrentUserId() async => firebaseAuth.currentUser!.uid;
+
+  @override
+  Stream<List<UserEntity>> getSingleUser(String userId) {
+    final userCollection = firebaseFirestore
+        .collection(FirebaseConst.users)
+        .where('userId', isEqualTo: userId)
+        .limit(1);
+    return userCollection.snapshots().map((querySnapshot) =>
+        querySnapshot.docs.map((e) => UserModel.fromSnapshot(e)).toList());
+  }
+
+  @override
+  Stream<List<UserEntity>> getUsers(UserEntity user) {
+    final userCollection = firebaseFirestore.collection(FirebaseConst.users);
+    return userCollection.snapshots().map((querySnapshot) =>
+        querySnapshot.docs.map((e) => UserModel.fromSnapshot(e)).toList());
+  }
+
+  @override
+  Future<bool> isSignIn() async => firebaseAuth.currentUser?.uid != null;
+
+  @override
+  Future<void> signInUser(UserEntity user) async {
+    try {
+      if (user.email!.isNotEmpty || user.password!.isNotEmpty) {
+        await firebaseAuth.signInWithEmailAndPassword(
+            email: user.email!, password: user.password!);
+      } else {
+        print('fields cannot be empty');
+      }
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        toast('user not found');
+      } else if (e.code == 'wrong-password') {
+        toast('Invalid email or password');
+      }
+    }
+  }
+
+  @override
+  Future<void> signOut() async {
+    await firebaseAuth.signOut();
+  }
+
+  @override
+  Future<void> signUpUser(UserEntity user) async {
+    try {
+      await firebaseAuth.createUserWithEmailAndPassword(
+          email: user.email!, password: user.password!).then((value) async {
+            if(value.user?.uid != null){
+              await createUser(user);
+            }
+      });
+      return;
+    } on FirebaseAuthException catch (e) {
+      if(e.code == 'email-already-in-use'){
+        toast('email is already taken');
+      } else {
+        toast('something went wrong');
+      }
+    }
+  }
+
+  @override
+  Future<void> updateUser(UserEntity user)async {
+    final userCollection = firebaseFirestore
+        .collection(FirebaseConst.users);
+    Map<String, dynamic> userInformation = Map();
+
+    if(user.userName != '' && user.userName != null) userInformation['userName'] = user.userName;
+
+    if (user.website != "" && user.website != null) userInformation['website'] = user.website;
+
+    if (user.profileUrl != "" && user.profileUrl != null) userInformation['profileUrl'] = user.profileUrl;
+
+    if (user.bio != "" && user.bio != null) userInformation['bio'] = user.bio;
+
+    if (user.name != "" && user.name != null) userInformation['name'] = user.name;
+
+    if (user.totalFollowing != null) userInformation['totalFollowing'] = user.totalFollowing;
+
+    if (user.totalFollowers != null) userInformation['totalFollowers'] = user.totalFollowers;
+
+    if (user.totalPosts != null) userInformation['totalPosts'] = user.totalPosts;
+
+    userCollection.doc(user.userId).update(userInformation);
+
+  }
+}
