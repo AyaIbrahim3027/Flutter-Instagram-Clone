cc52bb37ff47a24a04edb92b9b639604a5091381
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2023-12-13 16:48:05 +0200

    create getUsers methode

---
+++ lib/Features/presentation/cubit/user/user_cubit.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:instagram_clone/Features/domain/use%20cases/firebase_usecases/user/get_users_usecase.dart';
+import 'package:instagram_clone/Features/domain/use%20cases/firebase_usecases/user/update_user_usecase.dart';
+
+import '../../../domain/entities/user/user_entity.dart';
+
+part 'user_state.dart';
+
+class UserCubit extends Cubit<UserState> {
+  UserCubit({required this.updateUserUseCase, required this.getUsersUseCase})
+      : super(UserInitial());
+
+  final UpdateUserUseCase updateUserUseCase;
+  final GetUsersUseCase getUsersUseCase;
+
+  Future<void> getUsers({required UserEntity user}) async {
+    emit(UserLoading());
+    try {
+      final streamResponse = getUsersUseCase.call(user);
+      streamResponse.listen((users) {
+        emit(UserLoaded(users: users));
+      });
+    } on SocketException catch (e) {
+      emit(UserFailure());
+    } catch (e){
+      emit(UserFailure());
+    }
+  }
+}
