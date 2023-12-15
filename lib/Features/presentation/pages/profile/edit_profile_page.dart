e68fc91820ff9fdfa5627e1f4f99afac4c99089e
AyaIbrahim3027 <ayaibrahim3027@gmail.com>
2023-12-15 17:34:32 +0200

    add updateUserProfileDat method to edit profile

--- lib/Features/presentation/pages/profile/edit_profile_page.dart
+++ lib/Features/presentation/pages/profile/edit_profile_page.dart
@@ -1,11 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:instagram_clone/Features/domain/entities/user/user_entity.dart';
+import 'package:instagram_clone/Features/domain/use%20cases/firebase_usecases/storage/upload_image_to_storage_usecase.dart';
 import 'package:instagram_clone/Features/presentation/cubit/user/user_cubit.dart';
 import 'package:instagram_clone/Features/presentation/pages/profile/widgets/profile_form_widget.dart';
 import 'package:instagram_clone/consts.dart';
 
 import '../../../../profile_widget.dart';
+import 'package:instagram_clone/injection_container.dart' as di;
 
 class EditProfilePage extends StatefulWidget {
   const EditProfilePage({super.key, required this.currentUser});
@@ -33,6 +38,24 @@ class _EditProfilePageState extends State<EditProfilePage> {
     super.initState();
   }
 
+  File? image;
+  Future selectImage() async {
+    try {
+      final pickedFile = await ImagePicker.platform.getImageFromSource(
+        source: ImageSource.gallery,
+      );
+      setState(() {
+        if (pickedFile != null) {
+          image = File(pickedFile.path);
+        } else {
+          print('no image has been selected');
+        }
+      });
+    } catch (e) {
+      toast('some error occurred $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,7 +79,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
           Padding(
             padding: const EdgeInsets.only(right: 5),
             child: IconButton(
-              onPressed: updateUserProfile,
+              onPressed: updateUserProfileData,
               icon: const Icon(
                 Icons.done,
                 size: 32,
@@ -83,18 +106,22 @@ class _EditProfilePageState extends State<EditProfilePage> {
                     borderRadius: BorderRadius.circular(60),
                     child: profileWidget(
                       imageUrl: widget.currentUser.profileUrl,
+                      image: image,
                     ),
                   ),
                 ),
               ),
               sizeVer(15),
-              const Center(
-                child: Text(
-                  'Change profile photo',
-                  style: TextStyle(
-                    color: blueColor,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400,
+               Center(
+                child: GestureDetector(
+                  onTap: selectImage,
+                  child: const Text(
+                    'Change profile photo',
+                    style: TextStyle(
+                      color: blueColor,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 ),
               ),
@@ -139,7 +166,17 @@ class _EditProfilePageState extends State<EditProfilePage> {
     );
   }
 
-  updateUserProfile() {
+  updateUserProfileData(){
+    if(image == null){
+      updateUserProfile('');
+    } else {
+      di.sl<UploadImageToStorageUseCase>().call(image!, false, 'profileImages').then((profileUrl){
+        updateUserProfile(profileUrl);
+      });
+    }
+  }
+
+  updateUserProfile(String profileUrl) {
     setState(() {
       isUpdating = true;
     });
@@ -151,6 +188,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
             userName: usernameController!.text,
             website: websiteController!.text,
             bio: bioController!.text,
+            profileUrl: profileUrl,
           ),
         )
         .then((value) => clear());
